c772fd772332385a32ab55ff090d13b72a21583f
Blunacy <bornlunacy@naver.com>
2023-06-29 09:10:08 +0900

    Initial commit

---
+++ src/main/java/com/lunacy/secondforest/SecondForestApplication.java
@@ -0,0 +1,13 @@
+package com.lunacy.secondforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondForestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecondForestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lunacy/secondforest/SecondForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunacy.secondforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondForestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
